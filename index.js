8e7273fb15054818c912b4bf2f292256f8b7a1c2
surya <surya950302@gmail.com>
2020-11-27 21:33:51 -0500

    Exception/Error logging done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
 const moviesRoute = require('./routes/movies');
-
+const winston = require('winston');
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,17 +11,33 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+//create a logger
+const logger = winston.createLogger({
+	level:'info',
+	transports:[
+		new winston.transports.Console({
+			format:winston.format.combine(
+				winston.format.colorize({all:true})
+			)
+		}),
+		new winston.transports.File({filename: 'error.log', level:'error'})
+	],
+	exceptionHandlers:[
+		new winston.transports.File({filename:'exceptions.log'})
+	]
+});
+
 //routes
 app.use('/api/movies',moviesRoute);
 
 //connect to mongo db
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true}).then(() =>{
-	console.log("Connected to MongoDB Atlas")
+	logger.info("Connected to MongoDB Atlas")
 }).catch(error =>{
-	console.log("Something wrong happened",error);
+	logger.error(error.message);
 });
 
 // start server
 app.listen(PORT, () => {
-	console.log(`Server is running on Port ${PORT}`);
+	logger.info(`Server is running on Port ${PORT}`);
 })
--- routes/movies.js
+++ routes/movies.js
@@ -23,4 +23,41 @@ router.post('/',async (req,res) =>{
 	});
 });
 
+//GET: get all books
+router.get('/',(req,res) =>{
+	Movie.find()
+	.then((movies) => res.send(movies))
+	.catch((error) => {
+		res.status(500).send("Something went wrong")
+	})
+});
+
+//GET: get Movie by ID
+router.get('/:id', async (req,res) =>{
+	const movie = await Movie.findById(req.params.id);
+	if(!movie) res.status(404).send("Movie not Found");
+	res.send(movie);
+});
+
+//PUT: Update Movie based on ID
+router.put('/:id', async (req,res) =>{
+	const movie = await Movie.findByIdAndUpdate(req.params.id,{
+		name:req.body.movieName,
+		director:{
+			name:req.body.directorName,
+			age:req.body.directorAge
+		},
+		genre:req.body.genre
+	},
+	{ new:true});
+	if(!movie) res.status(404).send("Movie not Found");
+	res.send(movie);
+});
+
+//DELETE: delete Movie based on ID
+router.delete('/:id', async (req,res) =>{
+	const movie = await Movie.findByIdAndRemove(req.params.id);
+	if(!movie) res.status(404).send("Movie not Found");
+	res.send(movie);
+});
 module.exports = router;
